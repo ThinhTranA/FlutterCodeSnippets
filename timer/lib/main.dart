a01f25ffb6d0c995d6bc87106b88f92af10b29c0
Thinh Tran <48557139+ThinhTranA@users.noreply.github.com>
2019-10-10 21:29:19 +0800

    timer

--- timer/lib/main.dart
+++ timer/lib/main.dart
@@ -1,27 +1,115 @@
-
 import 'package:flutter/material.dart';
+import 'dart:async';
 
-void main() => runApp(MyApp());
+void main() => runApp(TimerApp());
 
-class MyApp extends StatelessWidget {
+class TimerApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return new TimerAppState();
+  }
+}
+
+class TimerAppState extends State<TimerApp> {
+  static const duration = const Duration(seconds: 1);
+
+  int secondsPassed = 0;
+  bool isActive = false;
+
+  Timer timer;
+
+  void handleTick() {
+    if (isActive) {
+      setState(() {
+        secondsPassed = secondsPassed + 1;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    if (timer == null)
+      timer = Timer.periodic(duration, (Timer t) {
+        handleTick();
+      });
+
+    int seconds = secondsPassed % 60;
+    int minutes = secondsPassed ~/ 60;
+    int hours = secondsPassed ~/ (60 * 60);
+
     return MaterialApp(
-      title: 'Timer',
+      title: 'Welcome to Flutter',
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Welcome to Flutter'),
+          title: Text('Timer'),
         ),
         body: Center(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
             children: <Widget>[
-              Text('0'),
-              Text('0'),
-              Text('0'),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  CustomTextContainer(
+                      label: 'HRS', value: hours.toString().padLeft(2, '0')),
+                  CustomTextContainer(
+                      label: 'MIN', value: minutes.toString().padLeft(2, '0')),
+                  CustomTextContainer(
+                      label: 'SEC', value: seconds.toString().padLeft(2, '0')),
+                ],
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 20),
+                child: RaisedButton(
+                  child: Text(isActive ? 'STOP' : 'START'),
+                  onPressed: () {
+                    setState(() {
+                      isActive = !isActive;
+                    });
+                  },
+                ),
+              ),
             ],
-          )
+          ),
         ),
+      ),
+    );
+  }
+}
+
+class CustomTextContainer extends StatelessWidget {
+  CustomTextContainer({this.label, this.value});
+
+  final String label;
+  final String value;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 5),
+      padding: EdgeInsets.all(20),
+      decoration: new BoxDecoration(
+        borderRadius: new BorderRadius.circular(10),
+        color: Colors.black87,
+      ),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          Text(
+            '$value',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 54,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Text(
+            '$label',
+            style: TextStyle(
+              color: Colors.white70,
+            ),
+          )
+        ],
       ),
     );
   }

8e624a5858adc92587b531b253d1d2a370a67af2
Thinh Tran <48557139+ThinhTranA@users.noreply.github.com>
2019-10-12 20:08:36 +0800

    Load data from NewsAPI and displayin in a listview

--- news_reader/lib/main.dart
+++ news_reader/lib/main.dart
@@ -1,111 +1,136 @@
+import 'dart:async';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
 import 'package:flutter/material.dart';
+import 'package:news_reader/Model/model.dart';
 
-void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+String API_KEY = 'b6897ddeb169416e8c99dd75d12aadec';
+Future<List<Source>> fetchNewsSource() async{
+  final response = await http.get('https://newsapi.org/v2/sources?apiKey=${API_KEY}');
+
+  if(response.statusCode == 200) //HTTP OK
+    {
+    List sources = json.decode(response.body)['sources'];
+    return sources.map((source) => new Source.fromJson(source)).toList();
+  }else{
+    throw Exception('Failed to load source list');
+  }
+
+}
+
+void main() => runApp(new SourceScreen());
+
+class SourceScreen extends StatefulWidget{
+
+  var list_sources;
+  var refreshKey = GlobalKey<RefreshIndicatorState> ();
+
+  @override
+  State<StatefulWidget> createState() => SourcesScreenState();
+}
+
+class SourcesScreenState extends State<SourceScreen>{
+
+  var list_sources;
+  var refreshKey = GlobalKey<RefreshIndicatorState>();
+
+  @override
+  void initState(){
+    super.initState();
+    refreshListSource();
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
+      title: 'News reader',
+      theme: ThemeData(primarySwatch: Colors.teal),
+      home: Scaffold(
+        appBar: AppBar(title: Text('Free NEWS'),),
+        body: Center(
+          child: RefreshIndicator(
+            child: FutureBuilder<List<Source>>(
+              future: list_sources,
+              builder: (context, snapshot){
+                if(snapshot.hasError)
+                  {
+                    Text('Error: ${snapshot.error}');
+                  }
+                else if(snapshot.hasData)
+                  {
+                    List<Source> sources = snapshot.data;
+                    return new ListView(
+                      children: sources.map((sources)=> GestureDetector(
+                      onTap: (){
+                        // TODO: implement
+                      },
+                        child: Card(
+                          elevation: 1.0,
+                          color: Colors.white,
+                          margin: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 4.0),
+                          child: Row(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Container(
+                                margin: const EdgeInsets.symmetric(horizontal: 2.0),
+                                width: 100.0,
+                                height: 140.0,
+                                child: Image.asset("assets/news.png")
+                              ),
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+                              Expanded(
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: <Widget>[
+                                    Row(
+                                      children: <Widget>[
+                                        Expanded(
+                                          child: Container(
+                                            margin: const EdgeInsets.only(top: 20.0, bottom: 10.0),
+                                            child: Text('${sources.name}', style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),),
+                                          ),
+                                        )
+                                      ],
+                                    ),
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
+                                    Container(
+                                      child: Text('${sources.description}', style: TextStyle(color: Color.fromARGB(255, 128, 128, 128), fontSize: 12.0, fontWeight: FontWeight.bold),),
+                                    ),
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+                                    Container(
+                                      child: Text('Category: ${sources.category}', style: TextStyle(color: Colors.black, fontSize: 14.0, fontWeight: FontWeight.bold),),
+                                    )
 
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
+                                  ],
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                        )).toList());
+                  }
+                return CircularProgressIndicator();
+              },
+            )
+            , onRefresh: refreshListSource),
+          ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+      );
+
   }
+
+  Future<Null> refreshListSource() async {
+    refreshKey.currentState?.show(atTop: false);
+
+    setState(() {
+      list_sources = fetchNewsSource();
+    });
+
+    return null;
+  }
+
+
 }
